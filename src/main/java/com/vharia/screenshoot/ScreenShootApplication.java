d4fad381411d1a6cbe7d312195556e730744ff01
Viral Haria <viralhar@gmail.com>
2020-05-23 20:52:01 +0530

    first commit - blank springBoot app

---
+++ src/main/java/com/vharia/screenshoot/ScreenShootApplication.java
@@ -0,0 +1,13 @@
+package com.vharia.screenshoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenShootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenShootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vharia/screenshoot/ScreenShootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vharia.screenshoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenShootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
